568b372c0e0066e5743b2b4c2ff347288747c2c6
drdynscript <drdynscript@gmail.com>
2018-04-24 22:00:58 +0200

    Rename content-navigation to content__navigation and add CSS for content__navigation

--- assets/js/content-navigation.js
+++ assets/js/content-navigation.js
@@ -7,13 +7,13 @@ function ready(cb) {
 ready(_ => {
   const ContentNavigation = {
       init() {
-          this._$contentNavigation = document.querySelector('.content-navigation');
+          this._$contentNavigation = document.querySelector('.content__navigation');
           this.generateContentNavigation();
       },
       generateContentNavigation() {
           const $headingElements = document.querySelectorAll('.content__body > * > * > * > h2, .content__body > * > * > * > h3, .content__body > * > * > * > h4');
           if ($headingElements != null && $headingElements.length > 0) {
-              let tempStr = '<ul class="navigation__list">';
+              let tempStr = '<ol class="content__navigation-list">';
               let previousLevel = null;
               $headingElements.forEach(($element, index) => {
                 const id = $element.getAttribute('id');
@@ -22,25 +22,25 @@ ready(_ => {
                 const currentLevel = parseInt($element.tagName.substring(1));
                 if(index > 0 && index < $headingElements.length - 1) {
                   if(currentLevel > previousLevel) {
-                    tempStr += `<ul>`;
+                    tempStr += `<ol>`;
                   } else if(currentLevel < previousLevel) {
                     for(let n=0;n<previousLevel-currentLevel;n++) {
-                      tempStr += `</li></ul>`;
+                      tempStr += `</li></ol>`;
                     }
                   } else {
                     tempStr += `</li>`;
                   }
                 }
-                tempStr += `<li class="navigation__item"><a class="pagemap__link" href="#${id}" title="${htmlTitle}">${html}</a>`;
+                tempStr += `<li class="content__navigation-item"><a class="pagemap__link" href="#${id}" title="${htmlTitle}">${html}</a>`;
                 if(index === $headingElements.length - 1) {
                   for(let n=0;n<previousLevel-currentLevel;n++) {
-                    tempStr += `</li></ul>`;
+                    tempStr += `</li></ol>`;
                   }
                 } else {
                   previousLevel = currentLevel;
                 }
               });
-              tempStr += '</ul>';
+              tempStr += '</ol>';
               this._$contentNavigation.innerHTML = tempStr;
           }
       }
